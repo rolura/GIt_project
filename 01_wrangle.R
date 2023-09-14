41edb3bff3adb7e3bec18516d2a919bff45f9d4c
Rosa Luna <rosaluna95@outlook.com>
2023-09-14 12:44:13 -0700

    changes made

--- 01_wrangle.R
+++ 01_wrangle.R
@@ -1,3 +1,22 @@
 #September 14th class
 
 print("hello")
+
+git_sitrep()
+
+#tells you what you never want to commit and their location
+git_vaccinate()
+#Do not share your token
+create_github_token()
+gitcreds::gitcreds_set() #add the token you just created
+
+#Run git_sitrep again to see what have changes
+git_sitrep()
+
+#Project does not uses Github so run this command
+use_github()
+
+#if you change something you need to commit the changes and click the push buttom on the ennvironment-git
+
+
+
--- gg.R
+++ gg.R
@@ -1,1 +1,109 @@
 #Workshop
+
+#Rositas-iMac:GIt_project rositaluna$ git status
+#On branch main
+
+#No commits yet
+
+#Untracked files:
+  #(use "git add <file>..." to include in what will be committed)
+#.gitignore
+#GIt_project.Rproj
+
+#nothing added to commit but untracked files present (use "git add" to track)
+#Rositas-iMac:GIt_project rositaluna$ library(usethis)
+#bash: syntax error near unexpected token `usethis'
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ git add .gitignore
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ git comit
+git: 'comit' is not a git command. See 'git --help'.
+
+The most similar command is
+        commit
+Rositas-iMac:GIt_project rositaluna$ get commit
+bash: get: command not found
+Rositas-iMac:GIt_project rositaluna$ git commit
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ git commit
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        gg.R
+
+nothing added to commit but untracked files present (use "git add" to track)
+Rositas-iMac:GIt_project rositaluna$ git commit
+On branch main
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        gg.R
+
+nothing added to commit but untracked files present (use "git add" to track)
+Rositas-iMac:GIt_project rositaluna$ git add
+Nothing specified, nothing added.
+hint: Maybe you wanted to say 'git add .'?
+hint: Turn this message off by running
+hint: "git config advice.addEmptyPathspec false"
+Rositas-iMac:GIt_project rositaluna$ git add .gitignore
+Rositas-iMac:GIt_project rositaluna$ git commit
+On branch main
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        gg.R
+
+nothing added to commit but untracked files present (use "git add" to track)
+Rositas-iMac:GIt_project rositaluna$ git add Git_project.Rproj
+Rositas-iMac:GIt_project rositaluna$ git commit
+On branch main
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        gg.R
+
+nothing added to commit but untracked files present (use "git add" to track)
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        gg.R
+
+nothing added to commit but untracked files present (use "git add" to track)
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ git add gg.R
+Rositas-iMac:GIt_project rositaluna$ git commit
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+        modified:   gg.R
+
+no changes added to commit (use "git add" and/or "git commit -a")
+Rositas-iMac:GIt_project rositaluna$ git add gg.R
+Rositas-iMac:GIt_project rositaluna$ git commit --m "Add R project"
+[main 1273099] Add R project
+ 1 file changed, 1 insertion(+)
+Rositas-iMac:GIt_project rositaluna$ git status
+On branch main
+nothing to commit, working tree clean
+Rositas-iMac:GIt_project rositaluna$ 
